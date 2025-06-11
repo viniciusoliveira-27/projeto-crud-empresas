0b781e92bca53b07fd281f83550f5bc49deb8aa4
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-06-10 23:55:31 -0300

    feat: Schema e DTO's concluidos

--- src/empresa/dto/update-empresa.dto.ts
+++ src/empresa/dto/update-empresa.dto.ts
@@ -1,4 +1,51 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEmpresaDto } from './create-empresa.dto';
+import { IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateEmpresaDto extends PartialType(CreateEmpresaDto) {}
+export class UpdateEmpresaDto {
+
+    @IsNotEmpty()
+    readonly id: number;
+    
+    @IsString()
+    @IsNotEmpty()
+    readonly nome: string;
+
+    @IsNotEmpty()
+    @IsString()
+    cnpj: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    telefone: string;
+
+    @IsNotEmpty()
+    @IsString()
+    endereco: string;
+
+    @IsNotEmpty()
+    @IsString()
+    cidade: string;
+
+    @IsNotEmpty()
+    @IsString() 
+    estado: string;
+
+    @IsNotEmpty()
+    @IsString()
+    cep: string;
+
+    @IsNotEmpty()
+    @IsString()
+    pais: string;
+
+    @IsNotEmpty()
+    @IsString() 
+    status: boolean; // assume que o status é uma string representando um booleano, como "true" ou "false"
+
+    
+
+    // Remove the last invalid decorator usage
+}
